bf72be089105d913e3e6cd497adc9673909d1000
andreeaCcss <andreeaccss@gmail.com>
2019-10-08 20:40:54 +0200

    Update room

--- index.js
+++ index.js
@@ -59,7 +59,10 @@ io.on("connection", function(socket) {
 
   socket.on("joinedRoom", function(params) {
     socket.join(params.id);
+    socket.in(params.id).emit("CreatePeer");
     socket.leave(socket.id);
+    //socket.emit(socket.id);
+    socket.in(socket.id).emit("Disconnect");
     console.log("socket", params.id);
   });
 });
