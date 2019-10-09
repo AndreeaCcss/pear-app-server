7ad791a11da0efa716ea01b59c016f0b40e5745e
andreeaCcss <andreeaccss@gmail.com>
2019-10-09 17:32:44 +0200

    Add delete route

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const jwt = require("express-jwt");
 const jwks = require("jwks-rsa");
-const { join } = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -40,16 +39,10 @@ const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 const io = require("socket.io")(server);
 io.origins("*:*");
 
-let clients = 0;
-
 io.on("connection", function(socket) {
   console.log("got connection", socket.id);
 
   socket.on("NewClient", function(data) {
-    // if (clients < 2) {
-    //  if (clients == 1) {
-    // this.emit("CreatePeer");
-
     console.log("rooms before leaving", socket.rooms);
     console.log("socketId before leaving", socket.id);
     socket.leave(socket.id, () => {
@@ -62,38 +55,14 @@ io.on("connection", function(socket) {
     });
 
     socket.on("disconnect", () => Disconnect(socket, data.roomId));
-
-    // socket.emit("CreatePeer");
-    //   }
-    // } else this.emit("SessionActive");
-    // clients++;
   });
 
   socket.on("Offer", SendOffer);
   socket.on("Answer", SendAnswer);
-  // socket.on("disconnect", () => Disconnect(socket, data.roomId));
-
-  // socket.on("changeToRoom", function(params) {
-  //   socket.leave(socket.id);
-  //   socket.join(params.roomId);
-
-  //   // socket.in(params.id).emit("CreatePeer");
-  //   // socket.leave(socket.id);
-  //   //socket.emit(socket.id);
-  //   // socket.in(socket.id).emit("Disconnect");
-  //   console.log("socket", params.id);
-  // });
 });
 
 function Disconnect(socket, roomId) {
-  // if (clients > 0) {
-  //   if (clients <= 2) {
-  // console.log("data Disconnect", data);
-  // console.log("this Disconnect rooms", this.rooms);
   socket.in(roomId).emit("Disconnect");
-  //   }
-  //   clients--;
-  // }
 }
 
 function SendOffer(offer) {
@@ -107,10 +76,3 @@ function SendAnswer(data) {
   const rooms = Object.keys(this.rooms);
   this.in(rooms[rooms.length - 1]).emit("BackAnswer", data);
 }
-
-// const nsp = io.of("/my-namespace");
-// nsp.on("connection", function(socket) {
-//   console.log("someone connected");
-// });
-
-// nsp.emit("hi", "everyone!");
